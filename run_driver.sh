776525026b706058a5b26f6fb93f6b6ce2e1971b
shivam <iamchauhan55@gmail.com>
2017-11-28 01:54:56 -0800

    Bash script to install driver and perform some pre-installation steps.

--- run_driver.sh
+++ run_driver.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
+
+#clear the terminal
 clear
+
+#navigate to the desired folder which has driver code
 cd ~/Documents/CMPE220/CMPE-220
+
+#delete the existing file
 rm log.txt
+
+#cleaing the make to start fresh
 make clean
+
+#Uninstall the existing driver if it is installed
 sudo rmmod myUSBdrive
+
+#make the driver and supporting files
 make
+
+#install the driver
 sudo insmod myUSBdrive.ko
 
