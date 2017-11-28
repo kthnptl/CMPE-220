7776d4b9fa355f7a4a127b89d0c4710a0dbf106c
shivam <iamchauhan55@gmail.com>
2017-11-28 02:00:28 -0800

    Bash script to run file operations and record the log in text file.

--- run_driver_fops.sh
+++ run_driver_fops.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
+
+
 #nautilus --browser /dev/ &
 
+#check if there is any file with a word demo in it. Which indicates if USB device is insterted or not.
 if find /dev/ -name "demo*" -print -quit | grep -q .
 then
 
+#if usb device is inserted then try to read from it.
 sudo cat /dev/demo*
+
+#try to write a character in it
 #sudo echo 'a' >> /dev/demo*
+
+#try to copy a text file in that file
 sudo cp myUSBdrive.c /dev/demo*
+
 echo "\n-------------------------------------------------------------------------------------" >> log.txt
 echo "--------------------------- " $(date) $(-rfc-2822) "---------------------------" >> log.txt
 echo "-------------------------------------------------------------------------------------\n" >> log.txt
+
+#copy all the log messages with CMPE tag into log file
 dmesg | grep -i CMPE >> log.txt
+
+#open that log file
 gedit log.txt &
 
 fi
